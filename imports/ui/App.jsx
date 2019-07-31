0e88a103b45e9962ac74b27424a45d24fb3d7bbe
Stephan Meijer <stephan.meijer@gmail.com>
2019-07-31 17:35:06 +0200

    break IE11 by adding an import statement

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Hello from './Hello.jsx';
 import Info from './Info.jsx';
 
+// the line below breaks IE. Comment/remove it to make the app render
+import { compose } from 'ramda';
+
 const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
